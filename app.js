3e0ad57fdee37cfb7cf0f4af1825ee864944eee2
Phanthat Muenprap <phanthat54@gmail.com>
2022-01-30 14:33:52 +0700

    create products.ejs

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 productRouter.route("/").get((req, res) => {
-    res.send('Hello product !!!!');
+    res.render("products");
 });
 
 productRouter.route("/1").get((req, res) => {
